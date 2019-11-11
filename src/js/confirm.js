ab508f7db83f36f07cfb085fb2bffd562fad34c5
dpws <dp@dpws.ru>
2019-11-11 16:43:37 +0300

    Added "Do not ask" checkbox to confirmation dialog

--- src/js/confirm.js
+++ src/js/confirm.js
@@ -2,11 +2,14 @@ import {closeTab, getAllTabs} from "./core/ChromeTools";
 
 import './core/shared';
 import '../assets/scss/style.scss';
-import {getOptions} from "./core/utils";
+import {getOptions, setOptions} from "./core/utils";
 
 window.addEventListener('load', async () => {
+    const tabs = await getAllTabs();
+    let currentOptions = {};
     getOptions()
         .then(options => {
+            currentOptions = options;
             if(!options.askConfirmation) {
                 for(let tab of tabs) {
                     closeTab(tab);
@@ -14,9 +17,15 @@ window.addEventListener('load', async () => {
                 parent.close();
             }
         });
-    const tabs = await getAllTabs();
     document.getElementById('tabs-count').textContent = tabs.length;
     document.getElementById('agree').addEventListener('click', () => {
+        const continueAsk = document.getElementById('stopAsking').checked;
+        if(continueAsk) {
+            setOptions({
+                ...currentOptions,
+                askConfirmation: false
+            })
+        }
         for(let tab of tabs) {
             closeTab(tab);
         }
